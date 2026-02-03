11dd0ef0b94d80abe83fa3e80a8f66659bdc85ca
SLUH Robotics <robotics@sluh.org>
2026-02-03 16:04:00 -0600

    Added to IntakeSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,34 +1,34 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.config.SparkMaxConfig;
-
-import java.nio.channels.Pipe;
-
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkMax;
 
-public class IntakeSubsystem extends SubsystemBase
-{
-    SparkFlex pickUpMotor = new SparkFlex(33,MotorType.kBrushless);
-    SparkMax tiltMotor = new SparkMax(32, MotorType.kBrushed);
+public class IntakeSubsystem extends SubsystemBase  {
+
     
-    public IntakeSubsystem()
-    {
-        SparkFlexConfig pickUpMotorConfig = new SparkFlexConfig();
-        SparkMaxConfig tiltMotorConfig = new SparkMaxConfig();
-    
-        pickUpMotorConfig.idleMode(IdleMode.kBrake);
-        pickUpMotorConfig.voltageCompensation(12);
-        pickUpMotorConfig.smartCurrentLimit(40);
+    SparkFlex intakeRoller = new SparkFlex(0, MotorType.kBrushless);
 
+    public IntakeSubsystem(){ 
 
+        SparkFlexConfig intakeRollerConfig = new SparkFlexConfig();
+        
 
-        tiltMotorConfig.idleMode(IdleMode.kBrake);
-    }
+        intakeRollerConfig.idleMode(IdleMode.kCoast);
+        intakeRollerConfig.smartCurrentLimit(40);
+        intakeRollerConfig.voltageCompensation(12);
+
+        intakeRoller.configure(intakeRollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }  
+
+    public void spinIntake(double speed) {
+
+        intakeRoller.set(speed);
+    } 
 }
